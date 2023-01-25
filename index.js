22cf9ea40e929eb0650cc34d264b4aaa7dd593ec
Pruthvi Acharya <pruthvi.acharya.54@gmail.com>
2023-01-25 22:42:23 +0100

    website

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-test
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Interactive Website</title>
+</head>
+<body>
+  <h1>My Interactive Website</h1>
+  <button id="button">Click Me</button>
+  <p id="text"></p>
+
+  <script>
+    // Select the button and text elements
+    var button = document.getElementById("button");
+    var text = document.getElementById("text");
+
+    // Add a click event listener to the button
+    button.addEventListener("click", function() {
+      text.innerHTML = "Hello, World!";
+    });
+  </script>
+</body>
+</html>
+
